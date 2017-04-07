384e258cd1a7c58e6567520ebe1ef1c340957d52
wingedgirl <wingedgirl@126.com>
2017-04-07 15:26:22 +0800

    TeamDemo1

---
+++ GitTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
