32514cb89f7e965368842a6342c9a1a92621a1a0
wingedgirl <wingedgirl@126.com>
2017-04-07 15:42:59 +0800

    输出能被2整除的1到100之间的数

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -12,7 +12,10 @@ namespace GitTest
         {
             for (int i = 1; i <= 100; i++)
             {
-                Console.WriteLine(i);
+                if(i%2==0)
+                {
+                    Console.WriteLine(i);
+                }
             }
 
 
